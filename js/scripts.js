b2df5dfe2237bbc9757bf7ad61a8d608906804ce
Maggie Summers and Sara Hamilton <sara@>
2018-01-18 10:45:59 -0800

    add ay to single vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 // biz logic
+var vowels = ["a", "e", "i", "o", "u"]
 var pigLatin = function(english) {
-  
-}
+  if (english.length === 1 && vowels.includes(english)) {
+    return english + "ay";
+  } else {
+    return ("Can't compute yet");
+  }
+};
 
 // party logic
 $(document).ready(function() {
