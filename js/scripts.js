53cbdf0db050130f252a27e6c146265d85faa13f
Maggie Summers and Sara Hamilton <sara@>
2018-01-18 11:44:05 -0800

    add leadingConsonants function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 // biz logic
 var vowels = ["a", "e", "i", "o", "u"]
+
+var leadingConsonants = function(english) {
+  for (var index = 0; !vowels.includes(english[index]); index += 1) {
+  }
+  var firstLetters = english.slice(0, index);
+  var middleLetters = english.slice(index);
+  return middleLetters + firstLetters + "ay";
+}
+
 var pigLatin = function(english) {
   if (english.length === 1 && vowels.includes(english)) {
     return english + "ay";
   } else if (vowels.includes(english.charAt(0))) {
     return english + "way";
-  } else {
+  } else if (!vowels.includes(english[0])) {
+    return leadingConsonants(english);
+  }
+  else {
     return ("Can't compute yet");
   }
 };
